0d01cb56022f498959562c09c06a7cf094066019
Atelier Ferrohenge <kai@ferrohenge.com>
2026-01-22 22:35:17 +0100

    Create required variables

--- src/property_def.h
+++ src/property_def.h
@@ -13,4 +13,13 @@ public:
 
 protected:
     static void _bind_methods();
+
+private:
+    StringName name;
+    StringName class_name;
+    Variant::Type type;
+    PropertyHint hint;
+    String hint_string;
+    // Review if this should be a type of Vector instead
+    TypedArray<PropertyUsageFlags> usage;
 };
