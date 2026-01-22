bf1bc7b9666de01f77b2ae182df743386b594fbc
Atelier Ferrohenge <kai@ferrohenge.com>
2026-01-22 22:44:53 +0100

    Define get set

--- src/property_def.h
+++ src/property_def.h
@@ -11,6 +11,21 @@ public:
     PropertyDef() = default;
     ~PropertyDef() override = default;
 
+    // Review if this should be a reference
+    void set_name(StringName p_name);
+    StringName get_name() const;
+    void set_class_name(StringName p_class_name);
+    StringName get_class_name() const;
+    void set_type(Variant::Type p_type);
+    Variant::Type get_type() const;
+    void set_hint(PropertyHint p_hint);
+    PropertyHint get_hint() const;
+    void set_hint_string(String p_hint_string);
+    String get_hint_string() const;
+    // Review if this should be a reference
+    void set_usage(TypedArray<PropertyUsageFlags> p_usage);
+    TypedArray<PropertyUsageFlags> get_usage() const;
+
 protected:
     static void _bind_methods();
 
