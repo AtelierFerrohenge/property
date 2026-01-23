02db7a243ce8023bc044580386d23adc5d6be29b
Atelier Ferrohenge <kai@ferrohenge.com>
2026-01-23 15:54:46 +0100

    Enable type limitations for hints

--- src/property_def.cpp
+++ src/property_def.cpp
@@ -16,14 +16,6 @@ StringName PropertyDef::get_class_name() const {
     return class_name;
 }
 
-void PropertyDef::set_hint(PropertyHint p_hint) {
-    hint = p_hint;
-}
-
-PropertyHint PropertyDef::get_hint() const {
-    return hint;
-}
-
 void PropertyDef::set_hint_string(String p_hint_string) {
     hint_string = p_hint_string;
 }
@@ -48,6 +40,14 @@ Variant::Type PropertyDef::get_type() const {
     return type;
 }
 
+void PropertyDef::set_hint(PropertyHint p_hint) {
+    hint = p_hint;
+}
+
+PropertyHint PropertyDef::get_hint() const {
+    return hint;
+}
+
 TypedArray<String> PropertyDef::get_valid_types() const {
     TypedArray<String> ret;
     if(GDVIRTUAL_CALL(_get_valid_types, ret)) {
@@ -57,35 +57,41 @@ TypedArray<String> PropertyDef::get_valid_types() const {
     return ret;
 }
 
+TypedArray<String> PropertyDef::get_valid_hints() const {
+    TypedArray<String> ret;
+    if(GDVIRTUAL_CALL(_get_valid_hints, ret)) {
+        return ret;
+    }
+    ret.push_back("None:0");
+    return ret;
+}
+
 void PropertyDef::_bind_methods() {
     ClassDB::bind_method(D_METHOD("set_name", "name"), &PropertyDef::set_name);
     ClassDB::bind_method(D_METHOD("get_name"), &PropertyDef::get_name);
     ClassDB::bind_method(D_METHOD("set_class_name", "class_name"), &PropertyDef::set_class_name);
     ClassDB::bind_method(D_METHOD("get_class_name"), &PropertyDef::get_class_name);
-    ClassDB::bind_method(D_METHOD("set_hint", "hint"), &PropertyDef::set_hint);
-    ClassDB::bind_method(D_METHOD("get_hint"), &PropertyDef::get_hint);
     ClassDB::bind_method(D_METHOD("set_hint_string", "hint_string"), &PropertyDef::set_hint_string);
     ClassDB::bind_method(D_METHOD("get_hint_string"), &PropertyDef::get_hint_string);
     ClassDB::bind_method(D_METHOD("set_usage", "usage"), &PropertyDef::set_usage);
     ClassDB::bind_method(D_METHOD("get_usage"), &PropertyDef::get_usage);
 
     GDVIRTUAL_BIND(_get_valid_types);
+    GDVIRTUAL_BIND(_get_valid_hints);
 
     ADD_PROPERTY(PropertyInfo(Variant::STRING_NAME, "name"), "set_name", "get_name");
     ADD_PROPERTY(PropertyInfo(Variant::STRING_NAME, "class_name"), "set_class_name", "get_class_name");
-    // Turn this into an enum with every member of Variant::Type, limited by a virtual method
-    // May need _get_property_list, may not if I can find a way with statics
-    ADD_PROPERTY(PropertyInfo(Variant::INT, "type"), "set_type", "get_type");
-    // Same as above
-    ADD_PROPERTY(PropertyInfo(Variant::INT, "hint"), "set_hint", "get_hint");
     ADD_PROPERTY(PropertyInfo(Variant::STRING, "hint_string"), "set_hint_string", "get_hint_string");
     // Determine if I can remove duplicates too
     ADD_PROPERTY(PropertyInfo(Variant::ARRAY, "usage", PROPERTY_HINT_ARRAY_TYPE, vformat("%s/%s:%s", Variant::INT, PROPERTY_HINT_ENUM, "To,Do,Later")), "set_usage", "get_usage");
 }
 
 bool PropertyDef::_set(const StringName &p_name, const Variant &p_value) {
+    // Potentially turn this into a switch
     if(p_name == StringName("type")) {
         set_type(static_cast<Variant::Type>(static_cast<int>(p_value)));
+    } else if(p_name == StringName("hint")) {
+        set_hint(static_cast<PropertyHint>(static_cast<int>(p_value)));
     } else {
         return false;
     }
@@ -95,6 +101,8 @@ bool PropertyDef::_set(const StringName &p_name, const Variant &p_value) {
 bool PropertyDef::_get(const StringName &p_name, Variant &r_ret) const {
     if(p_name == StringName("type")) {
         r_ret = get_type();
+    } else if(p_name == StringName("hint")) {
+        r_ret = get_hint();
     } else {
         return false;
     }
@@ -102,7 +110,9 @@ bool PropertyDef::_get(const StringName &p_name, Variant &r_ret) const {
 }
 
 void PropertyDef::_get_property_list(List<PropertyInfo> *p_list) const {
+    // Review if a bunch of push_backs is the right way to do this
     p_list->push_back(PropertyInfo(Variant::INT, "type", PROPERTY_HINT_ENUM, get_enum_hint(get_valid_types())));
+    p_list->push_back(PropertyInfo(Variant::INT, "hint", PROPERTY_HINT_ENUM, get_enum_hint(get_valid_hints())));
 }
 
 String PropertyDef::get_enum_hint(TypedArray<String> options) const {
--- src/property_def.h
+++ src/property_def.h
@@ -18,8 +18,6 @@ public:
     StringName get_name() const;
     void set_class_name(StringName p_class_name);
     StringName get_class_name() const;
-    void set_hint(PropertyHint p_hint);
-    PropertyHint get_hint() const;
     void set_hint_string(String p_hint_string);
     String get_hint_string() const;
     // Review if this should be a reference
@@ -28,9 +26,12 @@ public:
 
     void set_type(Variant::Type p_type);
     Variant::Type get_type() const;
+    void set_hint(PropertyHint p_hint);
+    PropertyHint get_hint() const;
 
-    // Maybe could be PackedStringArray?
+    // Maybe could be PackedStringArrays?
     virtual TypedArray<String> get_valid_types() const;
+    virtual TypedArray<String> get_valid_hints() const;
 
 protected:
     static void _bind_methods();
@@ -40,6 +41,7 @@ protected:
     void _get_property_list(List<PropertyInfo> *p_list) const;
 
     GDVIRTUAL0RC(TypedArray<String>, _get_valid_types)
+    GDVIRTUAL0RC(TypedArray<String>, _get_valid_hints)
 
 private:
     StringName name;
