fe0d4e450b01aa4ff603d48a228c08d5b9450cbf
Atelier Ferrohenge <kai@ferrohenge.com>
2026-01-24 21:38:07 +0100

    Split hint limiter types

--- src/property_limiter.cpp
+++ src/property_limiter.cpp
@@ -1,21 +1,42 @@
 #include "property_limiter.h"
 
+// Doesn't seem to update parent resources, figure out how to propagate up
+void PropertyLimiter::set_valid_string_hints(int p_hints) {
+    valid_string_hints = p_hints;
+}
+
+int PropertyLimiter::get_valid_string_hints() const {
+    return valid_string_hints;
+}
+
 // Doesn't seem to update parent resources, figure out how to
-void PropertyLimiter::set_valid_hints(int p_valid_hints) {
-    valid_hints = p_valid_hints;
+void PropertyLimiter::set_valid_standalone_hints(int p_hints) {
+    valid_standalone_hints = p_hints;
 }
 
-int PropertyLimiter::get_valid_hints() const {
-    return valid_hints;
+int PropertyLimiter::get_valid_standalone_hints() const {
+    return valid_standalone_hints;
 }
 
+// Will want to reveal or hide hint string field in usage depending on which source has used flags
 String PropertyLimiter::get_hints_enum_hint() const {
     // Use a reference if possible
-    PackedStringArray values = get_hint_defs();
+    PackedStringArray values = get_string_hint_defs();
     PackedStringArray result = {"None:0"};
     // Guard against invalid values for valid_hints
-    int flags = valid_hints;
+    int flags = valid_string_hints;
     int index = 0;
+    while(flags != 0) {
+        int bit = 1 << index;
+        if(flags & bit) {
+            result.push_back(values[index]);
+            flags &= ~bit;
+        }
+        ++index;
+    }
+    values = get_standalone_hint_defs();
+    flags = valid_standalone_hints;
+    index = 0;
     while(flags != 0) {
         int bit = 1 << index;
         if(flags & bit) {
@@ -28,30 +49,57 @@ String PropertyLimiter::get_hints_enum_hint() const {
 }
 
 void PropertyLimiter::_bind_methods() {
-    ClassDB::bind_method(D_METHOD("set_valid_hints", "valid_hints"), &PropertyLimiter::set_valid_hints);
-    ClassDB::bind_method(D_METHOD("get_valid_hints"), &PropertyLimiter::get_valid_hints);
+    ClassDB::bind_method(D_METHOD("set_valid_string_hints", "hints"), &PropertyLimiter::set_valid_string_hints);
+    ClassDB::bind_method(D_METHOD("get_valid_string_hints"), &PropertyLimiter::get_valid_string_hints);
+    ClassDB::bind_method(D_METHOD("set_valid_standalone_hints", "hints"), &PropertyLimiter::set_valid_standalone_hints);
+    ClassDB::bind_method(D_METHOD("get_valid_standalone_hints"), &PropertyLimiter::get_valid_standalone_hints);
 
-    ADD_PROPERTY(PropertyInfo(Variant::INT, "valid_hints", PROPERTY_HINT_FLAGS, get_hints_flags_hint()), "set_valid_hints", "get_valid_hints");
+    ADD_PROPERTY(PropertyInfo(Variant::INT, "valid_string_hints", PROPERTY_HINT_FLAGS, get_flags_hint(get_string_hint_defs())), "set_valid_string_hints", "get_valid_string_hints");
+    ADD_PROPERTY(PropertyInfo(Variant::INT, "valid_standalone_hints", PROPERTY_HINT_FLAGS, get_flags_hint(get_standalone_hint_defs())), "set_valid_standalone_hints", "get_valid_standalone_hints");
 }
 
-String PropertyLimiter::get_hints_flags_hint() {
+// Probably use reference here
+String PropertyLimiter::get_flags_hint(PackedStringArray defs) {
     PackedStringArray values;
-    for(const String &value : get_hint_defs()) {
+    for(const String &value : defs) {
         values.push_back(value.get_slice(":", 0));
     }
     return String(",").join(values);
 }
 
 // See if I can use constant references here instead
-PackedStringArray PropertyLimiter::get_hint_defs() {
-    // Overflows due to int size - need to split
+PackedStringArray PropertyLimiter::get_string_hint_defs() {
     return {
         "Range:1",
         "Enum:2",
         "Enum Suggestion:3",
         "Exp Easing:4",
-        "Link:5",
         "Flags:6",
+        "File:13",
+        "Global File:15",
+        "Resource Type:17",
+        // Can be "monospace" or "no_wrap" in latest
+        "Multiline Text:18",
+        "Placeholder Text:20",
+        "Object ID:22",
+        "Type String:23",
+        "Node Path Valid Types:26",
+        "Save File:27",
+        "Global Save File:28",
+        "Array Type:31",
+        "Dictionary Type:38",
+        "Node Type:34",
+        "Tool Button:39",
+        "Group Enable:42",
+        "Input Name:43",
+        "File Path:44",
+    };
+}
+
+// See if I can use constant references here instead
+PackedStringArray PropertyLimiter::get_standalone_hint_defs() {
+    return {
+        "Link:5",
         "Layers 2D Render:7",
         "Layers 2D Physics:8",
         "Layers 2D Navigation:9",
@@ -59,35 +107,16 @@ PackedStringArray PropertyLimiter::get_hint_defs() {
         "Layers 3D Physics:11",
         "Layers 3D Navigation:12",
         "Layers Avoidance:37",
-        "File:13",
         "Dir:14",
-        "Global File:15",
         "Global Dir:16",
-        "Resource Type:17",
-        "Multiline Text:18",
         "Expression:19",
-        "Placeholder Text:20",
         "Color No Alpha:21",
-        "Object ID:22",
-        "Type String:23",
-        "Node Path to Edited Node:24",
         "Object Too Big:25",
-        "Node Path Valid Types:26",
-        "Save File:27",
-        "Global Save File:28",
-        "Int is ObjectID:29",
         "Int is Pointer:30",
-        "Array Type:31",
-        "Dictionary Type:38",
         "Locale ID:32",
         "Localizable String:33",
-        "Node Type:34",
         "Hide Quaternion Edit:35",
         "Password:36",
-        "Tool Button:39",
         "Oneshot:40",
-        "Group Enable:42",
-        "Input Name:43",
-        "File Path:44",
     };
 }
--- src/property_limiter.h
+++ src/property_limiter.h
@@ -11,8 +11,10 @@ public:
     PropertyLimiter() = default;
     ~PropertyLimiter() override = default;
 
-    void set_valid_hints(int p_valid_hints);
-    int get_valid_hints() const;
+    void set_valid_string_hints(int p_hints);
+    int get_valid_string_hints() const;
+    void set_valid_standalone_hints(int p_hints);
+    int get_valid_standalone_hints() const;
 
     String get_hints_enum_hint() const;
 
@@ -20,9 +22,11 @@ protected:
     static void _bind_methods();
 
 private:
-    int valid_hints;
+    int valid_string_hints;
+    int valid_standalone_hints;
 
-    static String get_hints_flags_hint();
+    static String get_flags_hint(PackedStringArray defs);
 
-    static PackedStringArray get_hint_defs();
+    static PackedStringArray get_string_hint_defs();
+    static PackedStringArray get_standalone_hint_defs();
 };
