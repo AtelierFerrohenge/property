c9317d89d4a95919a0ad6352450153a81ac0045d
Atelier Ferrohenge <kai@ferrohenge.com>
2026-01-27 15:14:28 +0100

    Define property set features

--- src/property_set.h
+++ src/property_set.h
@@ -2,6 +2,8 @@
 
 #include "godot_cpp/classes/resource.hpp"
 
+#include "property_def.h"
+
 using namespace godot;
 
 class PropertySet : public Resource {
@@ -11,6 +13,16 @@ public:
     PropertySet() = default;
     ~PropertySet() override = default;
 
+    void set_properties(const TypedArray<PropertyDef> &p_properties);
+    TypedArray<PropertyDef> get_properties() const;
+
+    bool custom_set(const StringName &p_name, const Variant &p_value);
+    bool custom_get(const StringName &p_name, Variant &r_ret) const;
+    void custom_get_property_list(List<PropertyInfo> *p_list) const;
+
 protected:
     static void _bind_methods();
+
+private:
+    Vector<Ref<PropertyDef>> properties;
 };
